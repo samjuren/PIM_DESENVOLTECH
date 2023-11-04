5fb69218fa0bd9f3e0bc8c2bccd5135ead6ceb83
Samuel Juren <sammjureen@outlook.com>
2023-11-04 10:42:49 -0300

    Add project files.

---
+++ PIM_DESENVOLTECH/Controllers/ApontamentoHorasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PIM_DESENVOLTECH.Controllers
+{
+    public class ApontamentoHorasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PIM_DESENVOLTECH/Controllers/FeriasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PIM_DESENVOLTECH.Controllers
+{
+    public class FeriasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
