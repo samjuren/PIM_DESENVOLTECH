b287e38e8133d5033fc64b9dcb651b84f2252065
Matheus Borges <matheusborges7526@gmail.com>
2023-11-19 23:11:20 -0300

    Modificações 23:30

---
+++ PIM_DESENVOLTECH/Auxiliar/Isessao.cs
@@ -0,0 +1,13 @@
+﻿using PIM_DESENVOLTECH.Models;
+
+namespace PIM_DESENVOLTECH.Auxiliar
+{
+    public interface Isessao
+    {
+        void CriarSessaoDoUsuario(Funcionario funcionario);
+        void RemoverSessaoDoUsuario();
+        Funcionario BuscarSessaoDoUsuario();
+        void CriarSessaoDoUsuarioNome(string nome);
+
+    }
+}
