721f33524b53917a76763420880b59c8f9d3f37d
Matheus Borges <matheusborges7526@gmail.com>
2023-11-25 01:02:53 -0300

    Descontos

--- PIM_DESENVOLTECH/GerarFolhaServices/GerarFolha.cs
+++ PIM_DESENVOLTECH/GerarFolhaServices/GerarFolha.cs
@@ -9,31 +9,29 @@ namespace PIM_DESENVOLTECH.GerarFolhaServices
 
         private readonly Contexto _context;
 
-        public GerarFolha(Contexto context) 
+        public GerarFolha(Contexto context)
         {
-        
-         _context = context;
-        
+
+            _context = context;
+
         }
 
 
-        public void CalcularFolha() 
+        public void CalcularFolha()
         {
+
             var funcionarios = _context.Funcionario.ToList();
 
             foreach (var funcionario in funcionarios)
             {
-                _context.Entry(funcionario)
-                    .Reference(f => f.Descontos)
-                    .Load();
-
                 funcionario.CalcularDesconto();
                 funcionario.FolhaPagamentoConfirmada = true;
             }
 
             _context.SaveChanges();
-
         }
 
     }
+
+    
 }
